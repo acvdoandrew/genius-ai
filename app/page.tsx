0248a4069ed76abee35ea3941a4b98cfe85f3c22
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-09-20 17:24:10 -0400

    renders button in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p className="text-6xl text-green-500">Hello Genius AI</p>
+    <Button variant="destructive" size="lg">Click Me!</Button>
   )
 }
