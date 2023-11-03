1ecbcb9df23d80a034817a8120b4a567cea01901
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-11-03 13:53:52 -0400

    adds premium color variant to badge component

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -15,6 +15,7 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        premium: "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-primary-foreground border-0",
       },
     },
     defaultVariants: {
