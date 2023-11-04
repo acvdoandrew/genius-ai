50bcaec0e237d87a16725e5409c4e749f6feb48e
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-11-04 16:03:20 -0400

    finishes landing content

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
   return (
     <div className="h-full">
       <LandingNavbar />
       <LandingHero />
+      <LandingContent />
     </div>
   );
 }
