4a3cdb704d44f3655853d5a74b720fc09fb7a046
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-11-04 13:00:31 -0400

    finishes handling subscription with stripe

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { useState } from "react";
+import axios from "axios";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Check, Code, ImageIcon, MessageSquare, Music, VideoIcon, Zap } from "lucide-react";
 import { useProModal } from "@/hooks/use-pro-modal"
@@ -43,6 +45,20 @@ const tools = [
 
 export const ProModal = () => {
     const proModal = useProModal();
+    const [loading, setLoading] = useState(false);
+
+    const onSubscribe = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get("/api/stripe");
+
+            window.location.href = response.data.url;
+        } catch (error) {
+            console.log(error, "STRIPE_CLIENT_ERROR");
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -77,6 +93,7 @@ export const ProModal = () => {
                 </DialogHeader>
                 <DialogFooter>
                     <Button
+                        onClick={onSubscribe}
                         size="lg"
                         variant="premium"
                         className="w-full"
