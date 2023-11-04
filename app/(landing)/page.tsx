b75bee4eb45b1a15a12fee4fb0b8d246201f8350
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-11-04 15:03:16 -0400

    creates base landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
   return (
-    <div>
-        LandingPage (Unprotected)
-        <div>
-          <Link href="/sign-in">
-            <Button>
-              Login
-            </Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button>
-              Register
-            </Button>
-          </Link>
-        </div>
+    <div className="h-full">
+      <LandingNavbar /> 
     </div>
   );
 }
