cd44bde58a2374a593f788e4f8b01b6a8c52f8da
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-11-04 14:14:27 -0400

    removes tools limit for pro users

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { OpenAI } from "openai";
 
 import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
@@ -37,8 +38,9 @@ export async function POST(
         }
 
         const freeTrial = await checkApiLimit();
+        const isPro = await checkSubscription();
 
-        if (!freeTrial) {
+        if (!freeTrial && !isPro) {
             return new NextResponse("Free trial has expired.", { status: 403 });
         }
 
@@ -48,7 +50,9 @@ export async function POST(
             size: resolution,
         });
 
-        await increaseApiLimit();
+        if (!isPro) {
+            await increaseApiLimit();
+        }
 
         return NextResponse.json(response.data);
     } catch (error) {
